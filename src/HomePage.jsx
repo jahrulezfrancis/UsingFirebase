a3fbe886d64aef4ee2ee3d38be754de54eb23d75
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-12 16:53:44 +0100

    initial commit

---
+++ src/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+    return(
+        <div className="homePage">
+            <h2>bro I congratulate you on being able to land this page</h2>
+        </div>
+    )
+}
