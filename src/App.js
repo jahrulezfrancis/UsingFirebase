17efd11c49e816251501b5b506aaebf36d4bde80
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-12 16:52:51 +0100

    added app components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { WaitlistForm } from './Form';
+import { UserProvider } from './Helpers/ContextProvider';
+import HomePage from './HomePage';
+import { LoginForm } from './login';
 
 function App() {
   return (
     <div className="App">
+      <UserProvider>
+        <WaitlistForm />
+        <LoginForm />
+        <Routes>
+          <Route path='homepage' element={<HomePage />} />
+        </Routes>
+      </UserProvider>
     </div>
   );
 }
