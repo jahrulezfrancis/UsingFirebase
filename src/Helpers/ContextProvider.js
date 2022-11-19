a2d98b78ed56a6ba2fa08443b2624101200b38c8
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-19 23:41:18 +0100

    added useEffect method

--- src/Helpers/ContextProvider.js
+++ src/Helpers/ContextProvider.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, createContext } from "react"
+import { useState, createContext } from "react"
 export const UserContext = createContext()
 
 export function UserProvider({ children }) {
